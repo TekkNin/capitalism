0778bd71ef728c0d27fd3d810cc8457bb34f7753
Kevin Jordan <kevin@99summits.com>
2019-04-20 09:40:17 -0500

    Reduce trees in forest when worked.

---
+++ Capitalism/Capitalism.Logic/Handlers/UpdateForestHandler.cs
@@ -0,0 +1,20 @@
+﻿using Capitalism.Infrastructure.Repositories;
+using Capitalism.Logic.Events.BuildingEvents;
+using Capitalism.SharedKernel.Events;
+
+namespace Capitalism.Logic.Handlers
+{
+    public class UpdateForestHandler : IHandle<ForestWorkedEvent>
+    {
+        private readonly ITownBuildingRepository _townBuildingRepository;
+        public UpdateForestHandler(ITownBuildingRepository townBuildingRepository)
+        {
+            _townBuildingRepository = townBuildingRepository;
+        }
+
+        public void Handle(ForestWorkedEvent domainEvent)
+        {
+            _townBuildingRepository.Update(domainEvent.Forest);
+        }
+    }
+}
